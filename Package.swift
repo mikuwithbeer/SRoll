e7831806b4fb8c80909c3692815c39151513c809
mikuwithbeer <git@miku.beer>
2025-07-08 15:18:25 +0300

    create executable

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SRoll",
+    targets: [
+        .executableTarget(name: "SRoll")
+    ]
+)
