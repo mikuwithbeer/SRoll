22f66e0f92a9f714abec0ac4b2b5a62ec19b7da4
mikuwithbeer <git@miku.beer>
2025-07-08 18:30:43 +0300

    add simple keyboard sending for play

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,7 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-let data = try Config.load().select([])
+let data = try Config.load()
 
-let player = Player(data)
+let player = Player(data.select([]))
 try player.run()
+
+let input = Input(delay: data.inputDelay)
+input.send()
